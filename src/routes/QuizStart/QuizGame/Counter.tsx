13d57386c1a7f974761706e5af7396a5eb2b1dd2
Hyohyun Nam <skagygus8@gmail.com>
2022-07-21 23:16:37 +0900

    배포시도1

--- src/routes/QuizStart/QuizGame/Counter.tsx
+++ src/routes/QuizStart/QuizGame/Counter.tsx
@@ -7,14 +7,13 @@ const Counter = () => {
   const count = useCounter()
   const finalInfo = store.get('finalInfo')
   // console.log(typeof finalInfo)
-  useUnmount(() => {
-    store.get('finalInfo')
-  })
+  // useUnmount(() => {
+  //   store.get('finalInfo')
+  // })
 
   return (
     <div>
       <h2>{count}</h2>
-      <h3>{finalInfo.count}</h3>
     </div>
   )
 }
